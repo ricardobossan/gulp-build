c7327f74dbaa54028f40d7313d3efa6f4d417235
Ricardo Matos de Azevedo Bossan <ricardobossan@gmail.com>
2018-05-14 04:58:53 -0300

    add gulp-eslint, gulp-sass, gulp-autoprefixer, compile-sync funcionality to the gulpfile.js

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,4 @@
 (function() {
 	var foo = 123;
-	return /*?*/;
+	return foo;
 })();
--- gulpfile.js
+++ gulpfile.js
@@ -21,16 +21,29 @@ gulp.task('lint', () => {
 		.pipe(eslint.failAfterError());
 });
 
-gulp.task('styles', function() {
-	gulp.src('app/sass/*.scss')
+// Static Server + watching scss/html files
+gulp.task('serve', ['sass'], function() {
+
+	browserSync.init({
+		server: "./app"
+	});
+
+	gulp.watch("app/sass/*.scss", ['sass']);
+	gulp.watch("app/*.html").on('change', browserSync.reload);
+});
+
+// Compile sass into CSS & auto-inject into browsers
+gulp.task('sass', function() {
+	return gulp.src("app/sass/*.scss")
 		.pipe(sass().on('error', sass.logError))
 		.pipe(autoprefixer({
 			browsers: ['last 2 versions']
 		}))
-		.pipe(gulp.dest('app/css'));
+		.pipe(gulp.dest("app/css"))
+		.pipe(browserSync.stream());
 });
 
-gulp.task('default', ['lint'], function () {
-	gulp.watch('app/**/*.scss', ['styles']);
+
+gulp.task('default', ['lint', 'serve'], function () {
 	// This will only run if the lint task is successful...
 });
